cecc6e5795a080743cb9223a0ef774e8226743c0
Ian Kiku <iankiku@users.noreply.github.com>
2019-01-31 09:43:38 -0600

    reversing arrays

--- arrays_ds.py
+++ arrays_ds.py
@@ -1,6 +1,6 @@
 import array as arr  #array of numeric values are supported in Python by the array module
 
-sortd= arr.array('i', [1, 2,3,4,5])
+sortedList= arr.array('i', [1, 2,3,4,5])
 unsorted = arr.array('i', [4, 8, 2,5,7,1,3,6])
 
 count = 0
@@ -14,21 +14,48 @@ count = 0
 
 #  #print with while loop
 def printW(count):
-    while count < len(sortd):
-        print(sortd[count])
+    while count < len(sortedList):
+        print(sortedList[count])
         count = count +1
 
 
 # Print items in arrays with forloop
 def printFor():
-    for item in sortd:
+    for item in sortedList:
         print (item)
 # printFor()
 #  #-------------------------------------
+    
+#    
+def reversefn():
+    #   #reverse items in array using reverse iteration
+    
+    sortedList.reverse()
+    
+    
+    #   #Reversing a list in-place with the list.reverse() method
+    for i in sortedList:
+        print("Reverse() iter :", i )
+        
+    print("...\n")
+    
+    #   #Using the “[::-1]” list slicing trick to create a reversed copy
+    
+    sortedList[::-1]
+    for x in sortedList:
+        print("Reverse [::-1] slicing :", x )
+        
+    print("...\n")
+    
+    #   #Creating a reverse iterator with the reversed() built-in function
+    
+    for xy in reversed(unsorted):
+        print("reversed(): ", xy)
+        
+    print("...\n")
+    
+reversefn()
 
-#  #reverse items in array using reverse iteration
-def reverse():
-    pass
 
 # # #append items to array
 # def add():
